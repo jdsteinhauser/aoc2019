17e7f6002e51007fc16f441c7d4565d2eec2694b
Jason Steinhauser <jason.steinhauser@millgroupinc.com>
2019-12-08 00:05:58 -0600

    More refactoring

--- day07.exs
+++ day07.exs
@@ -1,36 +1,66 @@
 defmodule Computer do
 
-  defp get_value(0, position, data), do: Enum.at(data, position)
-  defp get_value(1, value, _), do: value
+  defp get(0, position, data), do: Enum.at(data, position)
+  defp get(1, value, _), do: value
 
   def run(idx, data, input, output) do
   
     inst = Enum.at(data, idx)
     opcode = rem(inst, 100)
     modes = div(inst, 100) |> Integer.digits() |> Enum.reverse()
-    
-    case {opcode, Enum.slice(data, idx + 1, 3)} do
-      # Opcode 1 - Add
-      {1, [x, y, pos | _]} -> run(idx + 4, List.replace_at(data, pos, get_value(Enum.at(modes, 0, 0), x, data) + get_value(Enum.at(modes, 1, 0), y, data)), input, output)
-      # Opcode 2 - Multiply
-      {2, [x, y, pos | _]} -> run(idx + 4, List.replace_at(data, pos, get_value(Enum.at(modes, 0, 0), x, data) * get_value(Enum.at(modes, 1, 0), y, data)), input, output)
-      # Opcode 3 - read input
-      {3, [pos | _]} -> run(idx + 2, List.replace_at(data, pos, hd(input)), tl(input), output)
-      # Opcode 4 - write output
-      {4, [pos | _]} -> run(idx + 2, data, input, output ++ [Enum.at(data, pos)])
-      # Opcode 5 - jump-if-true
-      {5, [x, pos | _]} -> run((if get_value(Enum.at(modes, 0, 0), x, data) != 0, do: get_value(Enum.at(modes, 1, 0), pos, data), else: idx + 3), data, input, output)
-      # Opcode 6 - jump-if-false
-      {6, [x, pos | _]} -> run((if get_value(Enum.at(modes, 0, 0), x, data) === 0, do: get_value(Enum.at(modes, 1, 0), pos, data), else: idx + 3), data, input, output)
-      # Opcode 7 - less than
-      {7, [x, y, pos | _]} -> run(idx + 4, List.replace_at(data, pos, (if get_value(Enum.at(modes, 0, 0), x, data) < get_value(Enum.at(modes, 1, 0), y, data), do: 1, else: 0)), input, output)
-      # Opcode 8 - equals
-      {8, [x, y, pos | _]} -> run(idx + 4, List.replace_at(data, pos, (if get_value(Enum.at(modes, 0, 0), x, data) === get_value(Enum.at(modes, 1, 0), y, data), do: 1, else: 0)), input, output)
-      # Opcde 99 - Halt
-      {99, _} -> {:ok, %{data: data, output: output}}
-      _ -> {:error, idx, data}
-    end
+
+    process(opcode, modes, idx, Enum.slice(data, idx + 1, 3), data, input, output)
   end
+
+  defp process(1, modes, idx, [x, y, pos | _], data, input, output) do  # Opcode 1 - Add
+    l = get(Enum.at(modes, 0, 0), x, data)
+    r = get(Enum.at(modes, 1, 0), y, data)
+    run(idx + 4, List.replace_at(data, pos, l+r), input, output)
+  end
+
+  defp process(2, modes, idx, [x, y, pos | _], data, input, output) do  # Opcode 2 - Multiply
+    l = get(Enum.at(modes, 0, 0), x, data)
+    r = get(Enum.at(modes, 1, 0), y, data)
+    run(idx + 4, List.replace_at(data, pos, l*r), input, output)
+  end
+
+  defp process(3, _modes, idx, [pos | _], data, input, output) do  # Opcode 3 - read input
+    run(idx + 2, List.replace_at(data, pos, hd(input)), tl(input), output)
+  end
+
+  defp process(4, _modes, idx, [pos | _], data, input, output) do  # Opcode 4 - write output
+    run(idx + 2, data, input, output ++ [Enum.at(data, pos)])
+  end
+
+  defp process(5, modes, idx, [x, pos | _], data, input, output) do  # Opcode 5 - jump-if-true
+    val = get(Enum.at(modes, 0, 0), x, data)
+    next_pos = if val != 0, do: get(Enum.at(modes, 1, 0), pos, data), else: idx + 3
+    run(next_pos, data, input, output)
+  end
+
+  defp process(6, modes, idx, [x, pos | _], data, input, output) do  # Opcode 6 - jump-if-false
+    val = get(Enum.at(modes, 0, 0), x, data)
+    next_pos = if val == 0, do: get(Enum.at(modes, 1, 0), pos, data), else: idx + 3
+    run(next_pos, data, input, output)
+  end
+
+  defp process(7, modes, idx, [x, y, pos | _], data, input, output) do  # Opcode 7 - less than
+    l = get(Enum.at(modes, 0, 0), x, data)
+    r = get(Enum.at(modes, 1, 0), y, data)
+    val = if l < r, do: 1, else: 0
+    run(idx + 4, List.replace_at(data, pos, val), input, output)
+  end
+
+  defp process(8, modes, idx, [x, y, pos | _], data, input, output) do  # Opcode 8 - equals
+    l = get(Enum.at(modes, 0, 0), x, data)
+    r = get(Enum.at(modes, 1, 0), y, data)
+    val = if l == r, do: 1, else: 0
+    run(idx + 4, List.replace_at(data, pos, val), input, output)
+  end
+
+  defp process(99, _, _, _, data, _, output), do: {:ok, %{data: data, output: output}}  # Opcode 99 - Halt
+  
+  defp process(_, _, idx, _, data, _, _), do: {:error, idx, data}
 end
 
 datums = File.read!("day07.txt") |> String.trim() |> String.split(",") |> Enum.map(&String.to_integer/1)
